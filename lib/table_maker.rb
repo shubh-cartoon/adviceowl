02f38e3282aa9d1571ff367913b175a2f369f41e
shubh-cartoon <shubhamgadiya7374@gmail.com>
2020-05-23 13:24:07 +0530

    Removed Index

--- lib/table_maker.rb
+++ lib/table_maker.rb
@@ -1,10 +1,8 @@
 class TableMaker < ActiveRecord::Migration[5.1]
   def make_a_table(table_name)
     create_table table_name do |t|
-        t.string :website_user_id, null: false
-        t.text :product_id,  null: false
-        add_index :website_user_id
-        add_index :product_id, :website_user_id
+        t.string :website_user_id
+        t.string :product_id
     end
   end
 end
