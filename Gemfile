0619caa1865551edc93a9fdbd22b9caef502b5fe
shubh-cartoon <shubhamgadiya7374@gmail.com>
2020-05-23 22:37:02 +0530

    Added Rake for heroku

--- Gemfile
+++ Gemfile
@@ -8,6 +8,7 @@ gem 'rails', '~> 5.2.4', '>= 5.2.4.3'
 # Use postgresql as the database for Active Record
 gem 'pg', '>= 0.18', '< 2.0'
 # Use Puma as the app server
+gem 'faker'
 gem 'puma', '~> 3.11'
 # Use SCSS for stylesheets
 gem 'sass-rails', '~> 5.0'
--- db/seeds.rb
+++ db/seeds.rb
@@ -6,3 +6,21 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
+require 'faker'
+
+users = []
+1000.times { users << Faker::Alphanumeric.alphanumeric(number: 10, min_alpha: 3, min_numeric: 3) }
+
+products = [] 
+20000.times { products << Faker::Alphanumeric.alphanumeric(number: 10, min_alpha: 3, min_numeric: 3) }
+
+10000000.times do
+  user = users[rand(0...1000)]
+  product = products[rand(0...20000)]
+  aval_products = ActiveRecord::Base.connection.execute("select * from company_e108e7bada0a146a38bc54709219fef4_data where website_user_id = '#{user}' and product_id = '#{product}'")
+  if aval_products.count > 0
+    next
+  else
+    add_products = ActiveRecord::Base.connection.execute("INSERT INTO company_e108e7bada0a146a38bc54709219fef4_data values ('#{user}', '#{product}')")
+  end
+end
